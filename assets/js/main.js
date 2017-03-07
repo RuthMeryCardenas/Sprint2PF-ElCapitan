7925787d419b1603c629af173fd4990fc77e6b99
RuthMery <ruth.cardenas.perez@gmail.com>
2017-03-06 22:46:26 -0500

    Creando index,main y style

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+//BOTONES
+  //Obteniendo los nodos "button" del HTML
+  var bSprint1 = document.getElementById('button1');
+  var bSprint2 = document.getElementById('button2');
+  var bSprint3 = document.getElementById('button3');
+  //Implementando las funciones CallBack del evento onclick de cada button
+  bSprint1.onclick = function () {
+    //Escribir código aqui
+  }
+  bSprint2.onclick = function () {
+    //Escribir código aqui
+  }
+  bSprint3.onclick = function () {
+    //Escribir código aqui
+  }
+//CARGA DEL FORMULARIO
+  //Implementando la función CallBack del evento onload
+  window.onload = function () {
+    bSprint2.click();
+  }
